d4130ba20d441e1f561d22fd19e50c88e2270a2b
ahmadbilal39515 <ahmadbilal39515@gmail.com>
2024-06-05 05:01:01 +0500

    debugging

--- app/workers/csv_export_worker.rb
+++ app/workers/csv_export_worker.rb
@@ -3,13 +3,14 @@ class CsvExportWorker
   include Sidekiq::Worker
 
   def perform(start_date, end_date)
+    puts "workeerrrrrrrrrrrrrrrrrrrrrrrr"
     start_date = start_date.to_date.beginning_of_day
     end_date = end_date.to_date.end_of_day
     products = Product.includes(sub_category: :category).where(created_at: start_date..end_date)
     csv_data = ProductExportService.to_csv(products)
     filename = "products_list_#{start_date.to_s}_#{end_date.to_s}.csv"
     temp_file_path = Rails.root.join('tmp', filename)
-
+    
     File.open(temp_file_path, 'w') do |file|
       file.write(csv_data)
       Rails.logger.info("CSV file created at #{temp_file_path}")
